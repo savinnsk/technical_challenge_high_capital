7a0f685518afda0e7d3dc3b02fee412eb6789a3d
savinnsk <saviodocuments@gmail.com>
2025-07-28 15:15:17 -0300

    feat(Api.Domain) : feat: add IUserRepository / IRepository interface

---
+++ backend/Api.Domain/Interfaces/Repositories/IUserRepository.cs
@@ -0,0 +1,9 @@
+using Api.Domain.Entities;
+
+namespace Api.Domain.Interfaces.Repositories
+{
+    public interface IUserRepository : IRepository<UserEntity>
+    {
+        Task<UserEntity> GetByEmail(string email);
+    }
+}
