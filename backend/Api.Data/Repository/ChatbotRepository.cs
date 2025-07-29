31c7584791e323714c965eaadab41d89c85f7a93
savinnsk <saviodocuments@gmail.com>
2025-07-29 10:41:17 -0300

    feat(Api.Data): add chatbot and message repository

---
+++ backend/Api.Data/Repository/ChatbotRepository.cs
@@ -0,0 +1,33 @@
+using Api.Data.Context;
+using Api.Domain.Entities;
+using Api.Domain.Interfaces.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace Api.Data.Repository
+{
+    public class ChatbotRepository : BaseRepository<ChatbotEntity>, IChatbotRepository
+    {
+        private DbSet<ChatbotEntity> _dataset;
+
+        public ChatbotRepository(MyContext context) : base(context)
+        {
+            _dataset = _context.Set<ChatbotEntity>();
+        }
+
+
+        public async Task<IEnumerable<ChatbotEntity>> GetChatbotsByUserIdAsync(Guid userId)
+        {
+            try
+            {
+                var result = await _dataset.ToListAsync();
+
+                return result;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
---
+++ backend/Api.Data/Repository/MessageRepository.cs
@@ -0,0 +1,32 @@
+using Api.Data.Context;
+using Api.Domain.Entities;
+using Api.Domain.Interfaces.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace Api.Data.Repository
+{
+    public class MessageRepository : BaseRepository<MessageEntity>, IMessageRepository
+    {
+        private DbSet<MessageEntity> _dataset;
+
+        public MessageRepository(MyContext context) : base(context)
+        {
+            _dataset = _context.Set<MessageEntity>();
+        }
+
+        public async Task<IEnumerable<MessageEntity>> GetMessagesByChatbotIdAsync(Guid chatbotId)
+        {
+            try
+            {
+                var result = await _dataset.ToListAsync();
+
+                return result;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
