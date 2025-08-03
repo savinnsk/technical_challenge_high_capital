523a532622922d195a719f927619f772a7b793d2
savinnsk <saviodocuments@gmail.com>
2025-08-03 13:31:19 -0300

    feat(Frontend): add provider

---
+++ frontend/src/providers/store-provider.tsx
@@ -0,0 +1,35 @@
+import { createContext, useCallback, useState } from 'react';
+
+interface StoreContextType {
+  error: boolean; 
+  toSetError: () => void;
+  notificationMessage: string | null; 
+  toSetNotification: (message: string) => void;
+}
+
+export const StoreContext = createContext<StoreContextType | undefined>(undefined);
+
+export const StoreProvider = ({ children }: { children: React.ReactNode }) => {
+  const [error, setError] = useState(false);
+  const [notificationMessage, setNotificationMessage] = useState<string | null>(null);
+
+  const toSetNotification = useCallback((message: string) => {
+    setNotificationMessage(message);
+    setTimeout(() => {
+      setNotificationMessage(null);
+    }, 3000);
+    setError(false);
+  }, []);
+
+  const toSetError = useCallback(() => {
+    setError(true);
+  }, []);
+
+  return (
+    <StoreContext.Provider
+      value={{ error, toSetError, notificationMessage, toSetNotification }}
+    >
+      {children}
+    </StoreContext.Provider>
+  );
+};
