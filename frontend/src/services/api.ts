f49e8ea7174eb26ab1fcf2ab1d994c5149c1407b
savinnsk <saviodocuments@gmail.com>
2025-08-03 13:48:44 -0300

    feat(Frontend): add api service and all calls http

---
+++ frontend/src/services/api.ts
@@ -0,0 +1,158 @@
+import axios from 'axios';
+
+export const authorization = async (formData: {
+    password: string,
+    email: string
+}) => {
+    try {
+        const response = await axios.post('http://localhost:5201/api/v1/Authorization', formData, {
+            headers: {
+                'Content-Type': 'multipart/form-data'
+            }
+        })
+
+
+        return response
+
+    } catch (error: any) {
+        return error
+    }
+}
+
+export const createUser = async (formData: {
+    password: string,
+    email: string,
+    neme : string
+}) => {
+    try {
+        const response = await axios.post('http://localhost:5201/api/v1/Users', formData, {
+            headers: {
+                'Content-Type': 'multipart/form-data'
+            }
+        })
+
+
+        return response
+
+    } catch (error: any) {
+        return error
+    }
+}
+
+export const getChatboots = async (token: string) => {
+    try {
+        const response = await axios.get(`http://localhost:5201/api/v1/Chatbots`, {
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `${token}`
+            }
+        })
+
+
+
+        return response
+
+    } catch (error: any) {
+
+        return error
+    }
+}
+
+export const getOneChatboots = async (data : {id : string , token: string}) => {
+    try {
+        const response = await axios.get(`http://localhost:5201/api/v1/Chatbots/${data.id}`, {
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `${data.token}`
+            }
+        })
+
+
+
+        return response
+
+    } catch (error: any) {
+
+        return error
+    }
+}
+
+export const deleteOneChatboots = async (data : {id : string , token: string}) => {
+    try {
+        const response = await axios.delete(`http://localhost:5201/api/v1/Chatbots/${data.id}`, {
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `${data.token}`
+            }
+        })
+
+
+
+        return response
+
+    } catch (error: any) {
+
+        return error
+    }
+}
+
+export const createChatboots = async (token: string, formData : {name: string,
+  context: string
+ }) => {
+    try {
+        const response = await axios.post(`http://localhost:5201/api/v1/Chatbots`,formData, {
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `${token}`
+            }
+        })
+
+
+
+        return response
+
+    } catch (error: any) {
+
+        return error
+    }
+}
+
+export const createMessage = async (token: string, formData : {chatBotId: string,
+  content: string
+ }) => {
+    try {
+        const response = await axios.post(`http://localhost:5201/api/v1/Messages`,formData, {
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `${token}`
+            }
+        })
+
+
+
+        return response
+
+    } catch (error: any) {
+
+        return error
+    }
+}
+
+export const getAllMessagesFromBot = async (token: string, chatBotId: string) => {
+    try {
+        const response = await axios.get(`http://localhost:5201/api/v1/Messages/${chatBotId}`, {
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `${token}`
+            }
+        })
+
+
+
+        return response
+
+    } catch (error: any) {
+
+        return error
+    }
+}
